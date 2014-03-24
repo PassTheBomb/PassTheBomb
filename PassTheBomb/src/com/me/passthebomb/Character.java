7f8135a06435afec2145084b204d32cf585fc624
FinalBlade <enwei_soh@mymail.sutd.edu.sg>
2014-03-24 12:28:17 +0800

    Character Class
    The abstract class for a character

---
+++ PassTheBomb/src/com/me/passthebomb/Character.java
@@ -0,0 +1,93 @@
+package com.passthebomb;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class Character {
+	private final int RADIUS = 32;
+
+	private Vector2 absPos;
+	private Circle circle;
+	private Texture circleImg;
+	private Background bg;
+	
+	private boolean carryingBomb;
+	private boolean alive;
+	
+	
+	protected Character(Vector2 absStartPos, Texture img, boolean bomb, Background bg) {
+		this.absPos=absStartPos;
+		this.circleImg = img;
+		this.carryingBomb = bomb;
+		this.bg = bg;
+		Vector2 temp = new Vector2(this.bg.getBackgroundPos().x,this.bg.getBackgroundPos().y);
+		this.alive = true;
+		this.circle = new Circle(temp.add(absStartPos), RADIUS);
+	}
+	
+	protected abstract void update();
+	
+	protected abstract void move(Vector3 tgtPos);
+	
+	protected abstract void collide(Character c);
+	
+	protected Circle getCharBox() {
+		return circle;
+	}
+	
+	protected Texture getCharImg() {
+		return circleImg;
+	}
+	
+	protected float getCharImgX(){
+		return circle.x - RADIUS;
+	}
+
+	protected float getCharImgY(){
+		return circle.y - RADIUS;
+	}
+	
+	protected void dispose() {
+		circleImg.dispose();
+	}
+
+	protected Vector2 getAbsPos() {
+		return absPos;
+	}
+
+	protected void setAbsPos(Vector2 absPos) {
+		this.absPos = absPos;
+		circle.x += bg.getBackgroundPos().x + absPos.x;
+		circle.y += bg.getBackgroundPos().y + absPos.y;
+	}
+
+	protected void setAbsPos(float x, float y) {
+		this.absPos.x = x;
+		this.absPos.y = y;
+		circle.x += bg.getBackgroundPos().x + absPos.x;
+		circle.y += bg.getBackgroundPos().y + absPos.y;
+	}
+	
+	protected Background getBg() {
+		return bg;
+	}
+
+	protected boolean isCarryingBomb() {
+		return carryingBomb;
+	}
+
+	protected void changeBombState(){
+		carryingBomb = !carryingBomb;
+	}
+
+	protected boolean isAlive() {
+		return alive;
+	}
+
+	protected void death() {
+		this.alive = false;
+	}
+	
+}
