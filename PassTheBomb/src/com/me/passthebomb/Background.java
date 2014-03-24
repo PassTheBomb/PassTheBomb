4b68da5e87d65d14b27e449e6e7e93b8e607591a
FinalBlade <enwei_soh@mymail.sutd.edu.sg>
2014-03-24 12:27:46 +0800

    Background Class
    Background scrolling and display is handled here

---
+++ PassTheBomb/src/com/me/passthebomb/Background.java
@@ -0,0 +1,59 @@
+package com.passthebomb;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class Background {
+	private static Background instance = null;
+	private final int LEFT_LIM = 0;
+	private final int RIGHT_LIM = -1248;
+	private final int TOP_LIM = -1568;
+	private final int BOTTOM_LIM = 0;
+	
+	private Texture bgImg;
+	private Vector2 bgPos;
+	
+	private Background(Texture bgImg, Vector2 startPos){
+		this.bgImg = bgImg;
+		this.bgPos = startPos;
+	}
+	
+	public static Background createBG(Texture bgImg, Vector2 startPos){
+		if (instance == null){
+			instance = new Background(bgImg, startPos);
+		}
+		return instance;
+	}
+	
+	protected void scrollX(float val){
+		bgPos.x += val;
+		if (bgPos.x < RIGHT_LIM){
+			bgPos.x = RIGHT_LIM;
+		}
+		else if (bgPos.x > LEFT_LIM){
+			bgPos.x = LEFT_LIM;
+		}
+	}
+	
+	protected void scrollY(float val){
+		bgPos.y += val;
+		if (bgPos.y < TOP_LIM){
+			bgPos.y = TOP_LIM;
+		}
+		else if (bgPos.y > BOTTOM_LIM){
+			bgPos.y = BOTTOM_LIM;
+		}
+	}
+	
+	protected void dispose(){
+		bgImg.dispose();
+	}
+	
+	protected Texture getBackgroundImg(){
+		return bgImg;
+	}
+	
+	protected Vector2 getBackgroundPos(){
+		return bgPos;
+	}
+}
