d7a92d6f165d58e3541bba86b5f3c7ed0bdcb51c
FinalBlade <enwei_soh@mymail.sutd.edu.sg>
2014-03-24 12:34:06 +0800

    Run the game
    Now the game can be run

--- PassTheBomb/src/com/me/passthebomb/PassTheBomb.java
+++ PassTheBomb/src/com/me/passthebomb/PassTheBomb.java
@@ -2,55 +2,78 @@ package com.me.passthebomb;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class PassTheBomb implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	private Vector3 touchPos;
+	private Background bg;
+	private Player player1;
+	private Opponent player2;
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+		// create new camera
+		camera = new OrthographicCamera();
+		// set camera to orthographic, size same as screen, y axis pointing up
+		camera.setToOrtho(false,800,480);
+
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		bg = Background.createBG(new Texture(Gdx.files.internal("background.jpg")), new Vector2(-624,-804));
+		player1 = new Player(new Vector2(774, 1044),new Texture(Gdx.files.internal("circle_r.png")), false, bg);
+		player2 = new Opponent(new Vector2(1274, 1044),new Texture(Gdx.files.internal("circle_r.png")), false, bg);
+
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
+		player1.dispose();
+		player2.dispose();
+		bg.dispose();
 	}
 
 	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+	public void render() {
+		// set the clear colour to r, g, b, a
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		// clear screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// update camera
+		camera.update();
+		// set SpriteBatch to camera coordinate system
 		batch.setProjectionMatrix(camera.combined);
+		// start new batch
 		batch.begin();
-		sprite.draw(batch);
+		// draw in the new batch
+		batch.draw(bg.getBackgroundImg(), bg.getBackgroundPos().x, bg.getBackgroundPos().y);
+		batch.draw(player1.getCharImg(), player1.getCharImgX(), player1.getCharImgY());
+		batch.draw(player2.getCharImg(), player2.getCharImgX(), player2.getCharImgY());
+		// end batch. **Note, all image rendering updates should go between
+		// begin and end
 		batch.end();
+		// acquire touch input
+		if (Gdx.input.isTouched()) {
+			// acquire touch position
+			touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+			// change position from global coordinates to camera coordinates
+			camera.unproject(touchPos);
+			player1.move(touchPos);
+			// update the cicle position
+			
+		}
+		player1.collide(player2);
+		player2.collide(player1);
+		player1.update();
+		player2.update();
 	}
+
 
 	@Override
 	public void resize(int width, int height) {
