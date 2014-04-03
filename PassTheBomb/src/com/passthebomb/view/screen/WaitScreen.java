a11651acc2a00bb2946baa1660ef4443fb17ae00
liuweilong <viclong1993@gmail.com>
2014-04-04 06:55:02 +0800

    Edited WaitScreen

--- PassTheBomb/src/com/passthebomb/view/screen/WaitScreen.java
+++ PassTheBomb/src/com/passthebomb/view/screen/WaitScreen.java
@@ -1,5 +1,14 @@
 package com.passthebomb.view.screen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
@@ -10,25 +19,31 @@ import com.passthebomb.model.local.Screen;
 
 public class WaitScreen implements com.badlogic.gdx.Screen{
 
+	final String HOST = "192.168.82.9";
+	final int PORT = 5432;
+	
 	private SpriteBatch batch;
 	private BitmapFont font;
-	private Screen lastScreen;
-	private MainMenuScreen screen;
-	private LobbyListScreen lobby;
+	private Socket socket;
+	private BufferedReader inChannel;
+	private int numOfPlayerJoined;
 	
 	public WaitScreen(com.badlogic.gdx.Screen lastScreen) {
 		batch = new SpriteBatch();    
         font = new BitmapFont();
         font.setColor(Color.RED);
-        if(lastScreen.getClass() == MainMenuScreen.class) {
-        	this.lastScreen = Screen.MAIN_MENU;
-        	this.screen = (MainMenuScreen)lastScreen;
-        } else {
-        	this.lastScreen = Screen.LOBBY;
-        	this.lobby = (LobbyListScreen)lastScreen;
-        }
-	}
-	
+        numOfPlayerJoined = 1;
+        try {
+			socket = new Socket(HOST, PORT);
+			inChannel = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }	
 	@Override
 	public void render(float delta) {
 		//Clear the screen
@@ -36,12 +51,19 @@ public class WaitScreen implements com.badlogic.gdx.Screen{
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         
 		batch.begin();
-		if(this.screen != null) {
-			font.draw(batch, this.screen.str, 250, 300);
-		} else {
-			font.draw(batch, this.lobby.str, 250, 300);
-		}
 		
+		try {
+			if(inChannel.ready()) {
+				numOfPlayerJoined = Integer.parseInt(inChannel.readLine());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		font.draw(batch, String.valueOf(numOfPlayerJoined), 250, 250);
+		if(numOfPlayerJoined == 4) {
+			ScreenManager.getInstance().show(Screen.GAME, this);
+		}
 		batch.end();
 	}
 
