33fd5540f1fc8723112ad7b58c9345bc44fc1127
Kevin Lee <kvnlte@gmail.com>
2014-04-09 02:14:52 +0800

    UDP was a lot more COMPLEX than I expected. The nature of UDP is that there is no lasting connection, unlike TCP. It is a temporary connection and that's it. Our server can host multiple games at the same time. So I needed to use some sort of list data structure to keep track of the game number so that the UDP packet data to the server can be routed to the correct game (client manager). I changed the Server code and GameScreen code, for udp input and output. Note that TCP IS STILL THERE so our important data can be preserved.
    I also made a couple of minor formatting tweaks to the code. Sorry if your print outs got removed haha. We will figure things out during debugging.
    
    I HAVE NOT TESTED the code. I tried running it once and it gives me UDP bind exception - stackoverflow told me to try on individual machines (server and clients should be on different machines).
    
    HOPE it works.
    
    (Note that if our server can only host ONE game, like bryan's group, then the changes that would be needed are trivial).
    P.S> Hon Boon given that I did the server previously, I thought it would be better if I integrated the UDP also.
    Would need everyone's help in testing !

--- PassTheBomb/src/com/passthebomb/Server.java
+++ PassTheBomb/src/com/passthebomb/Server.java
@@ -3,8 +3,12 @@ package com.passthebomb;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
@@ -17,14 +21,30 @@ import java.util.Random;
 public class Server {
 
 	static final int PLAYERS_PER_GAME = 4;
+	static final int PORT = 5432;
+
+	static int game_no = 0; // For each new game created, the ocunt increases by 1.
+
+	// Global list of ALL clients in all games.
+	// @Guarded by Server.class.
+	volatile static LinkedList<String> activeConnections; 
+
+	volatile static LinkedList<ClientManager> managers;
 
 	public static void main(String[] args) throws Exception {
-		
+
+		activeConnections = new LinkedList<String>();
+		managers = new LinkedList<ClientManager>();
+
 		// Suppress warning that serverSocket is never closed.
 		@SuppressWarnings("resource") 
-		ServerSocket serverSocket = new ServerSocket(5432);
+		ServerSocket serverSocket = new ServerSocket(PORT);
 		LinkedList<Socket> clientSockets = new LinkedList<Socket>();
 		ArrayList<PrintWriter> outArrayList = new ArrayList<PrintWriter>();
+		
+		// Start UDP Listener.
+		Thread udpListener = new UDP_Listener();
+		udpListener.start();
 
 		int count = 0; 
 
@@ -38,31 +58,50 @@ public class Server {
 			Socket socket = serverSocket.accept();
 			clientSockets.add(socket);
 			outArrayList.add(new PrintWriter(socket.getOutputStream(), true));
-			count++;
-			System.out.println(count + " p connected.");
+
+			String IP = socket.getInetAddress().toString();
 			
-			for(PrintWriter out : outArrayList) {
+			synchronized(Server.class) {
+				activeConnections.add(IP);
+			}
+			
+			System.out.println(count + " p connected to game number " + game_no 
+					+ " with IP: " + IP);
+
+			count++;
+
+			for (PrintWriter out : outArrayList) {
 				out.println(clientSockets.size());
 				System.out.println(out + ";" + count);
 			}
 
 			if (count == PLAYERS_PER_GAME) {
+				
+				// Generate list of IP addresses per game.
+				LinkedList<String> ipAddresses = new LinkedList<String>();
+				for (Socket s : clientSockets) {
+					ipAddresses.add(s.getInetAddress().toString());
+				}
 
 				// Create copy of the list of client sockets.
 				LinkedList<Socket> copyOfClientSockets = new LinkedList<Socket>(clientSockets);
 
-				// Pass the clientSockets to the clientManager.
-				Thread clientManager = new Thread(new ClientManager(copyOfClientSockets));
+				// Pass the clientSockets and their IPs to the clientManager, plus the game number.
+				ClientManager clientManager = new ClientManager(copyOfClientSockets, ipAddresses, game_no);
+				managers.add(clientManager);
+				Thread clientManagerThread = new Thread(clientManager);
 				System.out.println("Manager thread created.");
-				clientManager.start();
+				clientManagerThread.start();
 
 				// Reset count and clear the existing list of clients sockets. 
 				count = 0;
 				clientSockets.clear();
 				outArrayList.clear();
-			}
 
-			Thread.sleep(1000);
+				// Increment the game number.
+				game_no++; 
+
+			}
 		}
 	}
 }
@@ -70,31 +109,47 @@ public class Server {
 /**
  * Responsible for starting and managing client threads.
  * Receives client updates and pushes out information to all clients.
- * 
- * If performance is too slow, we may need to implement one dedicated thread for receiving updates, per client.
  */
 class ClientManager implements Runnable {
 
-	private LinkedList<Socket> clientSockets;
+	// udpBuffer is not synchronized for performance reasons;
+	// packet loss is not a big deal, which is why we use UDP in the first place.
+	private DatagramSocket udpSocket;
+	private volatile byte[] udpBuffer;			// UDP - frequent game updates over UDP.
+	private LinkedList<String> ipAddresses;
+	private LinkedList<Socket> clientSockets; 	// TCP - important messages over TCP only.
 	private LinkedList<BufferedReader> inputFromClients;
 	private LinkedList<PrintWriter> outputToClients;
 	private int size;
+	private int game_no;
+
 	private int bombTimer; 
 	private int bombHolder;
-	private boolean[] bombList = new boolean[Server.PLAYERS_PER_GAME];
+	private boolean[] bombList;
 
-	public ClientManager(LinkedList<Socket> clients) {
+	public ClientManager(LinkedList<Socket> clients, LinkedList<String> ipAddresses, int game_no) {
+		try {
+			udpSocket = new DatagramSocket(Server.PORT);
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+		}
+		udpBuffer = null;
+		this.ipAddresses = ipAddresses;
 		clientSockets = clients;
 		inputFromClients = new LinkedList<BufferedReader>();
 		outputToClients = new LinkedList<PrintWriter>();
 		size = Server.PLAYERS_PER_GAME;
+		this.game_no = game_no;
+		bombList = new boolean[Server.PLAYERS_PER_GAME];
 
-		// Set bomb timer to 60sec.
-		bombTimer = 60000;
+		// Set bomb timer to a minimum of baseTime with a variable extraTime 
+		Random randomExtraTime = new Random();
+		int baseTime = 30000; 								// 30 sec.
+		int extraTime = 1000 * randomExtraTime.nextInt(10); // +0-10 sec.
+		bombTimer = baseTime + extraTime;					// total time. 
 
 		try {
 			for (Socket s : clientSockets) {
-				System.out.println(s.isClosed());
 				inputFromClients.add(
 						new BufferedReader(
 								new InputStreamReader(s.getInputStream()) ));
@@ -107,21 +162,30 @@ class ClientManager implements Runnable {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Sets the buffer attribute in the ClientManager class.
+	 */
+	public void set(byte[] buf) {
+		udpBuffer = buf;
+	}	
 
 	@Override
 	public void run() {
 		try {
-			for (int i = 0; i < Server.PLAYERS_PER_GAME; i++){
+
+			for (int i = 0; i < size; i++) {
 				bombList[i] = false;
 			}
+
 			Random rand = new Random();
-			bombHolder = rand.nextInt(Server.PLAYERS_PER_GAME);
+			bombHolder = rand.nextInt(size);
 			bombList[bombHolder] = true;
-			
-			// Inform client of their id and who is the bomb holder. 
+
+			// Inform client of their id and who is the bomb holder over TCP. 
 			for (int i = 0; i < size; i++) {
 				inputFromClients.get(i).readLine();
-				
+
 				String initInfo = i + ";0,312,512," + bombList[0] + ";1,412,512," + bombList[1] + ";2,712,512," + bombList[2] + ";3,612,512," + bombList[3];
 				outputToClients.get(i).println(initInfo);
 			}
@@ -129,56 +193,114 @@ class ClientManager implements Runnable {
 			long startTime = System.currentTimeMillis();
 
 			// Receive client information and update all clients constantly.
+			// Uses UDP for performance reasons.
 			while (true) {
-				for (int i = 0; i < size; i++) {
-					
-					// Is the buffer ready to be read? If not, I'll check the next buffer.
-					if (inputFromClients.get(i).ready()) {
-						
-						/* Receive: 
-						 * 	"id, x_coordinate, y_coordinate, bomb_from, bomb_to" */
-						String in = inputFromClients.get(i).readLine();
-						System.out.println(in);
-						String input[] = in.split(",");
-						
-						
+				if (udpBuffer != null) { // Extract the stuff from the buffer.
+					String in = new String(udpBuffer, "UTF8");
+					udpBuffer = null; 
+					System.out.println(in);
+
+					String input[] = in.split(",");
+
+					try {
+						// Process packet information.
+						int playerNo = Integer.parseInt(input[0]);
 						int collidedPlayerNo = Integer.parseInt(input[3]);
 						boolean carryBomb = Boolean.parseBoolean(input[4]);
-						if (collidedPlayerNo != -1 && carryBomb && carryBomb == bombList[i]){
-							bombList[i] = false;
+						if (collidedPlayerNo != -1 && carryBomb && carryBomb == bombList[playerNo]) {
+							bombList[playerNo] = false;
 							bombList[collidedPlayerNo] = true;
 						}
-						//To-Do: Must handle collision checker. "Handshake" the collision
 						
-						// Transmit to all other clients.
-						for (int j = 0; j < size; j++) {
-							outputToClients.get(j).println(input[0]+","+input[1]+","+input[2]+","+bombList[i]);
+						// Generate output string to be sent to rest of clients.
+						String output = input[0]+","+input[1]+","+input[2]+","+bombList[playerNo];
+						byte[] outputBuffer = output.getBytes("UTF8");
+						
+						// Transmit packet to all other clients over UDP.
+						for (String ip : ipAddresses) {
+							udpSocket.send(
+									new DatagramPacket(
+											outputBuffer, 
+											outputBuffer.length, 
+											InetAddress.getByName(ip), 
+											Server.PORT));
 						}
+						
+					} catch (Exception e) { 
+						System.out.println("Error in processing UDP packet.");
 					}
 				}
-
+				
 				// Periodically check if bomb has expired then exit loop.
 				if (System.currentTimeMillis() - startTime >= bombTimer) {
 					System.out.println("Bomb Exploded.");
 					break;
 				}
 			}
-			
-			// Inform all clients that the bomb has exploded.
+
+			// After exiting loop, inform all clients that the bomb has exploded.
+			// Done over TCP.
 			for (int i = 0; i < size; i++) {
 				outputToClients.get(i).println("Exploded");
 			}
-			
-			// Perform clean up logic.
+
+			// Clean up and close TCP connections.
+			int index = game_no * 4; 
 			for (int i = 0; i < size; i++) {
 				outputToClients.get(i).close();
 				inputFromClients.get(i).close();
 				clientSockets.get(i).close();
+
+				synchronized(Server.class) {
+					// Replace the ips from the global list of connections so that
+					// the UDP Listener will not re-use them.
+					Server.activeConnections.set(index+i, "gameover"); 
+				}
+				
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+}
 
-	}	
+// Receives all UDP packets and redirects to the correct players in the correct game.
+class UDP_Listener extends Thread {
+
+	DatagramSocket socket;
+
+	UDP_Listener() {
+		try {
+			socket = new DatagramSocket(Server.PORT);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				byte[] buf = new byte[256];
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet); // Blocks till packet is received.
+
+				String clientIP = packet.getAddress().toString();
+				
+				/* Using the IP of the packet, determine the global client number
+				 * and hence determine the game number, so that we can "re-direct"
+				 * the packet to the correct game out of the many games running. */
+				
+				int globalClientNo = Server.activeConnections.indexOf(clientIP);
+				int game_no = (globalClientNo) / Server.PLAYERS_PER_GAME; // integer division
+
+				// Give the packet data (stored in the buffer) to the correct client manager.
+				Server.managers.get(game_no).set(buf);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- PassTheBomb/src/com/passthebomb/view/screen/GameScreen.java
+++ PassTheBomb/src/com/passthebomb/view/screen/GameScreen.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -25,33 +30,34 @@ import com.passthebomb.view.gui.Background;
 
 public class GameScreen implements Screen {
 	private final int[] screenSize = {1024,1024};
-	
+
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Vector3 touchPos;
 	private Background bg;
 	private Player player;
 	private Opponent[] oppList = new Opponent[4];
-	
+
 	private Touchpad touchpad;
 	private TouchpadStyle touchpadStyle;
 	private Skin touchpadSkin;
 	private Drawable touchBackground;
 	private Drawable touchKnob;
 	private Stage stage;
-	
-	volatile static String HOST = "192.168.82.9"; // MODIFY THIS FIELD AS REQUIRED.
+
+	volatile static String HOST = "192.168.1.132"; // MODIFY THIS FIELD AS REQUIRED.
 	static final int PORT = 5432;
 
+	DatagramSocket udpSocket;
 	Socket hostSocket;
 	private PrintWriter outputToHost;
 	private BufferedReader inputFromHost;
 	private String output;		// String output to server.
 	private String input; 		// String input from server.
 	private WaitScreen lastScreen;
-	
+
 	private int updateCtr = 0;
-	
+
 	private int id;				// Player id.
 	private int bombHolder; 	// Indicates the player id which holds the bomb.
 	private Vector3[] posList;
@@ -59,40 +65,39 @@ public class GameScreen implements Screen {
 
 	private Listener runnableListener;
 	private Thread listener;
-	
+
 	public GameScreen(com.badlogic.gdx.Screen lastScreen) {
 		this.lastScreen = (WaitScreen)lastScreen;
 		try {
 			// Set up connections and i/o streams.
+			udpSocket = new DatagramSocket(PORT);
 			hostSocket = this.lastScreen.getSocket();
 			outputToHost = new PrintWriter(hostSocket.getOutputStream(), true);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		runnableListener = new Listener(this.lastScreen.getSocket(), this);
 		listener = new Thread(runnableListener);
 		listener.start();
-		
+
 		try {
 			synchronized(this) {
-				this.wait();
+				this.wait(); 	// Wait for listener for initialization data.
 			}
 		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		id = runnableListener.getWhoAmI();
 		posList = runnableListener.getPositionList();
 		bombList = runnableListener.getBombList();
-		
-		//Setup with first line of input.
-			
-		
+
+		// Setup with first line of input.
+
 		batch = new SpriteBatch();
 		bg = Background.createBG(new Texture(Gdx.files.internal("background.png")), new Vector2(0,0), screenSize);
-		
+
 		Texture[] playerTexture = new Texture[2];
 		Texture[] oppTexture = new Texture[2];
 		playerTexture[0] = new Texture(Gdx.files.internal("player_r.png"));
@@ -100,16 +105,16 @@ public class GameScreen implements Screen {
 		oppTexture[0] = new Texture(Gdx.files.internal("opp_r.png"));	
 		oppTexture[1] = new Texture(Gdx.files.internal("opp_rb.png"));	
 		player = new Player(new Vector2(posList[id].x,posList[id].y), playerTexture, bombList[id], bg);
-		
-		for(int i = 0; i < 4; i++){
-			if (i != id){
+
+		for(int i = 0; i < 4; i++) {
+			if (i != id) {
 				oppList[i] =  new Opponent(new Vector2(posList[i].x,posList[i].y), oppTexture, bombList[i], bg);
 			}
-			else{
+			else {
 				oppList[i] = null;
 			}
 		}
-		
+
 		touchpadSkin = new Skin();
 		touchpadSkin.add("touchBackground", new Texture("touchBackground.png"));
 		touchpadSkin.add("touchKnob", new Texture("touchKnob.png"));
@@ -128,19 +133,19 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		camera.position.set(player.getAbsPos().x, player.getAbsPos().y, 0);
-		
+
 
 		posList = runnableListener.getPositionList();
 		bombList = runnableListener.getBombList();
-		
-		for(int i = 0; i < 4; i++){
-			if (i != id){
+
+		for(int i = 0; i < 4; i++) {
+			if (i != id) {
 				oppList[i].move(posList[i]);
 				oppList[i].setBomb(bombList[i]);
 			}
 		}
 		player.setBomb(bombList[id]);
-		
+
 
 		// set the clear colour to r, g, b, a
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
@@ -155,28 +160,28 @@ public class GameScreen implements Screen {
 		// draw in the new batch
 		batch.draw(bg.getBackgroundImg(), 0, 0);
 		batch.draw(player.getCharImg(), player.getCharImgX(), player.getCharImgY());
-		
-		for(int i = 0; i < 4; i++){
-			if (i != id){
+
+		for(int i = 0; i < 4; i++) {
+			if (i != id) {
 				batch.draw(oppList[i].getCharImg(), oppList[i].getCharImgX(), oppList[i].getCharImgY());
 			}
 		}
 		// end batch. **Note, all image rendering updates should go between
 		// begin and end
 		batch.end();
-		
+
 		stage.act(Gdx.graphics.getDeltaTime());	    
-	    stage.draw();
-	    
-	    
-	    touchPos = new Vector3(touchpad.getKnobPercentX()*10, touchpad.getKnobPercentY()*10, 0);
-		System.out.println("X: "+touchpad.getKnobPercentX());
-		System.out.println("Y: "+touchpad.getKnobPercentY());
-		System.out.println("Moving (GameScreen) X: "+(touchPos.x)+" Y:"+(touchPos.y));
+		stage.draw();
+
+
+		touchPos = new Vector3(touchpad.getKnobPercentX()*10, touchpad.getKnobPercentY()*10, 0);
+		//System.out.println("X: "+touchpad.getKnobPercentX());
+		//System.out.println("Y: "+touchpad.getKnobPercentY());
+		//System.out.println("Moving (GameScreen) X: "+(touchPos.x)+" Y:"+(touchPos.y));
 		// change position from global coordinates to camera coordinates
 		//camera.translate(x, y);
 		player.move(touchPos);
-		
+
 		/*// acquire touch input
 		if (Gdx.input.isTouched()) {
 			// acquire touch position
@@ -185,31 +190,43 @@ public class GameScreen implements Screen {
 			camera.unproject(touchPos);
 			player.move(touchPos);
 			// update the cicle position
-			
+
 		}*/
 		player.update();
-		
-		
+
+
 		int collidedTarget = -1;
-		for (int i = 0; i < 4; i++){
+		for (int i = 0; i < 4; i++) {
 			if (i != id){
-				if (player.collide(oppList[i])){
+				if (player.collide(oppList[i])) {
 					collidedTarget = i;
 				}
 			}
 		}
-		
-		
+
+
 		//compile message to send to server;
 		/*if (updateCtr < 2){
 			updateCtr++;
 		}
 		else{*/
-			outputToHost.println(id+","+player.getAbsPos().x+","+player.getAbsPos().y+","+collidedTarget+","+player.getBombState());
-			updateCtr = 0;
+		outputToHost.println();
+		updateCtr = 0;
 		//}
-	
-
+		
+		String output = id+","+player.getAbsPos().x+","+player.getAbsPos().y
+						+","+collidedTarget+","+player.getBombState();
+		try {
+			byte[] outputBuffer = output.getBytes("UTF8");
+			udpSocket.send(
+					new DatagramPacket(
+							outputBuffer, 
+							outputBuffer.length, 
+							InetAddress.getByName(GameScreen.HOST), 
+							GameScreen.PORT));
+		} catch (Exception e) { 
+			System.out.println(e);
+		}
 	}
 
 	@Override
@@ -222,59 +239,68 @@ public class GameScreen implements Screen {
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		player.dispose();
-		for (int i = 0 ; i < 4; i ++){
-			if (i != id){
+		for (int i = 0 ; i < 4; i ++) {
+			if (i != id) {
 				oppList[i].dispose();
 			}
 		}
 		bg.dispose();
 	}
 
-	
+
 }
 
-class Listener implements Runnable{
+class Listener implements Runnable {
 	private final static int PLAYER_LIMIT = 4;
-	
+
 	private BufferedReader inputFromHost;
 	private PrintWriter outputToHost;
 	private Socket socket;
-	
+	private DatagramSocket UDPSocket;
+
 	private volatile static int whoAmI = -1;
 	private volatile static Vector3[] positionList = new Vector3[PLAYER_LIMIT];
 	private volatile static boolean[] bombList = new boolean[PLAYER_LIMIT];
 
 	private GameScreen mainThread;
-	
-	public Listener(Socket socket, GameScreen mainThread){
+
+	public Listener(Socket socket, GameScreen mainThread) {
 		this.socket = socket;
 		this.mainThread = mainThread;
+
+		try {
+			UDPSocket = new DatagramSocket(GameScreen.PORT);
+			UDPSocket.setSoTimeout(1000);
+		} catch (SocketException e) {
+			System.out.println(e);
+		}
 	}
+
 	@Override
 	public void run() {
 		try {
@@ -283,11 +309,13 @@ class Listener implements Runnable{
 					new InputStreamReader(socket.getInputStream()));
 			outputToHost = new PrintWriter(socket.getOutputStream(),true);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println(e);
 		}
+		
 		outputToHost.println("ready");
-		System.out.println("READY RARRRT");
+		
 		try {
+			// Process initialization data over TCP.
 			String input = inputFromHost.readLine();
 			String[] passedInfo = input.split(";");
 			whoAmI = Integer.parseInt(passedInfo[0]);
@@ -301,53 +329,74 @@ class Listener implements Runnable{
 				positionList[player] = new Vector3(x,y,0);
 				bombList[player] = Boolean.parseBoolean(currentPlayerInfo[3]);
 			}
-			
+
 			synchronized(mainThread) {
 				mainThread.notify();	
 			}
-			
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		while(true){
+
+		while(true) {
 			try {
-				String[] passedInfo;
-				if (inputFromHost.ready()){
-					passedInfo = inputFromHost.readLine().split(",");
-					/*for (int i = 0; i < passedInfo.length; i++){
+				// Get UDP input from server, if any.
+				byte[] buf = new byte[256];
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+				try {
+					UDPSocket.receive(packet); // Blocks for the specified socket timeout.
+				} catch (SocketTimeoutException e) {
+					
+					// If no UDP packets are received with the timeout period, check for any TCP packet.
+					if (inputFromHost.ready()) {
+						if (inputFromHost.readLine().equals("Exploded")) {
+							
+							// Close connections.
+							inputFromHost.close();
+							outputToHost.close();
+							socket.close();
+
+							// TODO
+							// TODO
+							// TODO: Change screen to credit screen.
+							// For now, server doesn't send over who has the bomb. 
+							// If you have the bomb, you just lose immediately (:
+
+							break;
+						}
+					}
+				}
+
+				String input = new String(buf, "UTF8");
+				String[] passedInfo = input.split(",");
+
+				/*for (int i = 0; i < passedInfo.length; i++) {
 						System.out.println(passedInfo[i]);
 					}*/
-					int player = Integer.parseInt(passedInfo[0]);
-					float x = Float.parseFloat(passedInfo[1]);
-					float y = Float.parseFloat(passedInfo[2]);
-					positionList[player] = new Vector3(x,y,0);
-					bombList[player] = Boolean.parseBoolean(passedInfo[3]);;
-				}
-			} catch (IOException e) {
-				if (socket.isClosed()){
-					System.out.println("socket closed");
-				}
-				System.out.println("Unknown Error");
-				e.printStackTrace();
-			}
-			catch (Exception e){
-				System.out.println("RARRRR");
+				int player = Integer.parseInt(passedInfo[0]);
+				float x = Float.parseFloat(passedInfo[1]);
+				float y = Float.parseFloat(passedInfo[2]);
+				positionList[player] = new Vector3(x,y,0);
+				bombList[player] = Boolean.parseBoolean(passedInfo[3]);;
+			} 
+			
+			catch (Exception e) {
+				System.out.println(e);
 			}
 		}
-		
+
 	}
-	
-	public int getWhoAmI(){
+
+	public int getWhoAmI() {
 		return whoAmI;
 	}
-	
-	public Vector3[] getPositionList(){
+
+	public Vector3[] getPositionList() {
 		return positionList;
 	}
-	
-	public boolean[] getBombList(){
+
+	public boolean[] getBombList() {
 		return bombList;
 	}
 }
